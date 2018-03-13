1fb626106711fc081ef5c2dcda26f8b2ef1457d6
David Von Lehman <david.vonlehman@gmail.com>
2018-03-13 09:20:05 -0700

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+from pelican.readers import Markdown
+import markdown
+
+AUTHOR = u'David Von Lehman'
+SITENAME = u'PELICAN ACADEMIC'
+SITEURL = ''
+
+THEME = 'pelican-academic'
+PATH = 'content'
+
+LOAD_CONTENT_CACHE = False
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+THEME_TEMPLATES_OVERRIDES = ['templates']
+
+# Set this so autoreload will detect changes to templates
+STATIC_PATHS = ['templates']
+
+# PLUGIN_PATHS = ['themes/pelican-academic/plugins']
+# PLUGIN_PATHS = ['pelican-plugins']
+# PLUGINS = ['jinja2content']
+
+# DIRECT_TEMPLATES = ['index']
+# EXTRA_TEMPLATES_PATHS = ['templates']
+# TEMPLATE_PAGES = {'pages/index.html': 'index.html'}
+
+def markdown_convert(input):
+    print input
+    return 'FOOOOO' + markdown.markdown(input)
+
+JINJA_FILTERS = {
+    'markdown': markdown_convert,
+    'upper': lambda input:input.upper()
+}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+FEED_ALL_ATOM = 'atom.xml'
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+GOOGLE_FONT="Montserrat:400,700%7cRoboto:400,400italic,700%7cRoboto&#43;Mono"
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
